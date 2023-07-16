a6c92a8a6598771b0578002cd56f71e76e80d2e8
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-07-16 11:25:44 +0530

    greedy

--- src/ActivitySelection.java
+++ src/ActivitySelection.java
@@ -1,2 +1,32 @@
+import java.util.ArrayList;
+
 public class ActivitySelection {
+
+    public static int activitySelection(int[] start, int[] end) {
+        int maxAct = 1;
+        ArrayList<Integer> res = new ArrayList<>();
+        res.add(0);
+        int lastEnd = end[0];
+
+        for (int i = 1; i < end.length; i++) {
+            if (start[i] >= lastEnd) {
+                maxAct++;
+                res.add(i);
+                lastEnd = end[i];
+            }
+        }
+        for (Integer re : res) {
+            System.out.print("A" + re + " ");
+        }
+        System.out.println();
+        return maxAct;
+    }
+
+
+    public static void main(String[] args) {
+        int[] start = {1, 3, 0, 5, 8, 5};
+        int[] end = {2, 4, 6, 7, 9, 9};
+
+        System.out.println(activitySelection(start, end));
+    }
 }
