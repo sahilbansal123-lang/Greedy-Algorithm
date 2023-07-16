78e8813b3d450b88b8a5a9ec3d94d910413e47be
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-07-16 10:45:52 +0530

    greedy

--- src/Theory.java
+++ src/Theory.java
@@ -1,10 +1,16 @@
 public class Theory {
     public static void main(String[] args) {
         System.out.println(
-
-
-
-
+        "Greedy is an algorithmic paradigm that builds up a solution piece by piece, always choosing the next piece that \n" +
+        "offers the most obvious and immediate benefit. So the problems where choosing locally optimal also leads to \n" +
+        "global solution are the best fit for Greedy. \n"
+        );
+        System.out.println(
+        "The algorithm never reverses the earlier decision even if the choice is wrong. It works in a top-down approach.\n"
+        );
+        System.out.println(
+        "This algorithm may not produce the best result for all the problems. It's because it always goes for the local \n" +
+        "best choice to produce the global best result.\n"
         );
     }
 }
