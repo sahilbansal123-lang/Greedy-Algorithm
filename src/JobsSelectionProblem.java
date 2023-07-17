ca02206b680c15a459b48338be38e9e7cc27196e
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-07-17 12:35:53 +0530

    greedy

---
+++ src/JobsSelectionProblem.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class JobsSelectionProblem {
+
+    public static class Job {
+        int id;
+        int deadline;
+        int profit;
+
+        public Job(int i, int d, int p) {
+            id = i;
+            deadline = d;
+            profit = p;
+        }
+    }
+
+    public static void jobsSelection (int[][] jobsInfo){
+        ArrayList<Job> jobs = new ArrayList<>();
+        for (int i = 0; i < jobsInfo.length; i++) {
+            jobs.add(new Job(i, jobsInfo[i][0], jobsInfo[i][1]));
+        }
+        Collections.sort(jobs, (a,b) -> b.profit-a.profit);
+        
+        ArrayList<Integer> seq = new ArrayList<>();
+        int time = 0;
+        for (int i = 0; i < jobs.size(); i++) {
+            Job curr = jobs.get(i);
+            if (curr.deadline > time) {
+                seq.add(curr.id);
+                time++;
+            }
+        }
+
+        System.out.println("max jobs = " + seq.size());
+        for (int i = 0; i < seq.size(); i++) {
+            System.out.print(seq.get(i) + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[][] jobsInfo = {{4, 20}, {1, 10}, {1, 30}, {1, 40}};
+        jobsSelection(jobsInfo);
+    }
+}
